83630c7a4d3769e572e873fc4956d0060c318cbb
jorodrig96 <jo.rodrig96@gmail.com>
2024-01-29 19:27:55 -0800

    added middleware and added a home file to the views folder

---
+++ views/Home.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+import Default from './Default'
+
+export default function Home(){
+    return(
+        <Default>
+            <main>
+                <h1>Home Page</h1>
+            </main>
+        </Default>
+    )
+}
